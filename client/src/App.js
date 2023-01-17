b07ce63b233c2785c600abfb7c8e3e430b20dae3
brx <lucabrkovic@gmx.de>
2023-01-17 12:27:32 +0100

    1. Navbar frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
-
+import { ChakraProvider } from "@chakra-ui/react";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Navbar from "./components/Navbar"
 function App() {
   return (
-    <div>
-      Tech
-    </div>
+   <ChakraProvider>
+    <BrowserRouter>
+      <Navbar />
+      <main>
+   
+      </main>
+    </BrowserRouter>
+   </ChakraProvider>
   );
 }
 
