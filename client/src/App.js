decdc83d9f84602812f8e3f3881508242a714f75
brx <lucabrkovic@gmx.de>
2023-01-18 08:59:11 +0100

    2. Product Card

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Navbar from "./components/Navbar"
+import ProductsScreen from "./screen/ProductsScreen"
 function App() {
   return (
    <ChakraProvider>
     <BrowserRouter>
       <Navbar />
       <main>
-   
+      <Routes>
+      <Route path="/products" element={<ProductsScreen />}/>
+      </Routes>
       </main>
     </BrowserRouter>
    </ChakraProvider>
